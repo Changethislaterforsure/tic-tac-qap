823d196bc871b414d9829c1d38a581eacf9dba83
changethislaterforsure <benjmain.legge@keyin.comm>
2025-09-25 14:29:12 -0230

    Created Player class and updated gitignore to include LCK file I think

--- src/main/java/com/example/tictactoe/Player.java
+++ src/main/java/com/example/tictactoe/Player.java
@@ -1,5 +1,19 @@
 package com.example.tictactoe;
 
 public class Player {
-    
+    private final String name;
+    private final Marker marker;
+
+    public Player(String name, Marker marker) {
+        this.name = name;
+        this.marker = marker;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Marker marker() {
+        return marker;
+    }
 }
