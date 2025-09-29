ca9cbef74ca749125952b86b38f117f5fa9b0da3
changethislaterforsure <benjmain.legge@keyin.comm>
2025-09-29 15:07:52 -0230

    Created main class

--- src/main/java/com/example/tictactoe/Main.java
+++ src/main/java/com/example/tictactoe/Main.java
@@ -1,1 +1,9 @@
+package com.example.tictactoe;
 
+public class Main {
+    public static void main(String[] args) {
+        String p1 = args.length > 0 ? args[0] : "Player 1";
+        String p2 = args.length > 1 ? args[1] : "Player 2";
+        new Game(p1, p2).playLoop();
+    }
+}
